179fe6176bfc45dc1d787a2dc9d14d281a8c327d
isabel k. lee <hello@isabelklee.co>
2020-01-26 03:24:24 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,8 +1,4 @@
 first_number = 1 
 second_number = 2
 
-def sum(first_number, second_number)
-  puts first_number + second_number
-end
-
-sum(first_number, second_number)
+sum = first_number + second_number
